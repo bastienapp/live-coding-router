73baa2aab6cbcbf7791c5449a4eda164d45a0458
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-11-05 11:06:28 +0100

    add lorem ipsum

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,12 @@ function Home() {
   return (
     <section className="Home">
       <h1>Welcome to Wild Movies</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae quo
+        nesciunt consequuntur. Blanditiis maxime ratione, rerum quaerat dolorem
+        doloribus quis provident eveniet cupiditate velit corrupti sed,
+        voluptatibus praesentium neque necessitatibus?
+      </p>
     </section>
   );
 }
